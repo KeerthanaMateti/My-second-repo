b71ea76ac5b06db2243459ac9a40ba700dd69a7d
keerthana <keerthana.mateti@gmail.com>
2026-09-13 06:46:35 +0000

    added text in app.java

---
+++ app.java
@@ -0,0 +1,14 @@
+Name: Keerthana
+Role: Linux System Administrator
+
+Skills:
+- RHEL 7, 8, 9
+- Linux Server Administration
+- Server Patching
+- User and Group Management
+- Package Management
+- File System Management
+- LVM
+- Troubleshooting
+- VMware
+- Basic Shell Scripting
